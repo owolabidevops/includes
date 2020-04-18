d2125ce02c3076a72ea78209ef5f991f07bbd2a6
Owolabi <48913682+owolabidevops@users.noreply.github.com>
2020-04-18 13:21:44 +0100

    Add files via upload

---
+++ includes.js
@@ -0,0 +1,5 @@
+//the include method simply returns true or false after checking wether the given array contains the provided element or not
+
+const cars=['bmw','toyota','tesla','audi'];
+console.log(cars.includes('toyota'));//true
+console.log(cars.includes('mercedes'));//false
